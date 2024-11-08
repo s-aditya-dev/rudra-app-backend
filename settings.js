878c5ee0843e2151624eb4d63aa6d05a05faf994
s-aditya-dev <sraditya1203@gmail.com>
2024-11-09 00:32:25 +0530

    changes done in setting.js

---
+++ settings.js
@@ -0,0 +1,6 @@
+export const frontEndPort = "https://rudra-app-frontend.vercel.app";
+export const backEndPort = "https://rudra-app-backend.vercel.app";
+
+// local config.js
+// export const frontEndPort = "http://localhost:5173";
+// export const backEndPort = "http://localhost:8000";
